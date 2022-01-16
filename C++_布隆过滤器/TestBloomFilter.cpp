1af32bd603bb838e518a9c8d4b8c3c2310d69524
dodamce <88144913+dodamce@users.noreply.github.com>
2022-01-16 18:32:41 +0800

    C++利用位图实现简单的布隆过滤器

---
+++ C++_布隆过滤器/TestBloomFilter.cpp
@@ -0,0 +1,74 @@
+#include"BloomFilter.h"
+
+#include<iostream>
+using namespace std;
+
+void Test()
+{
+	BloomFilter<200>bits;
+	bits.Set("小红");
+	bits.Set("小刚");
+	bits.Set("小明");
+	if (bits.Find("小明"))
+	{
+		cout << "Yes" << endl;
+	}
+}
+
+void Test2()
+{
+	BloomFilter<400>bits;
+	vector<string>Vet;
+	int Size = 100;
+	for (int i = 0; i < Size; i++)
+	{
+		string Str = "NUC";
+		Str += to_string(2022 + i);
+		Vet.push_back(Str);
+	}
+	for (int i = 0; i < Vet.size(); i++)
+	{
+		bits.Set(Vet[i]);
+	}
+	bool IsError = false;
+	for (int i = 0; i < Vet.size(); i++)
+	{
+		if (bits.Find(Vet[i]) == false)
+		{
+			IsError = true;
+			break;
+		}
+	}
+	if (IsError == false)
+	{
+		cout << "Yes" << endl;
+	}
+	else
+	{
+		cout << "Error" << endl;
+	}
+
+	cout << "-----------------" << endl;
+	vector<string>Vet2;
+	for (int i = 0; i < Size; i++)
+	{
+		string Str = "Dod";
+		Str += to_string(2022 + i);
+		Vet2.push_back(Str);
+	}
+	
+	for (int i = 0; i < Vet.size(); i++)
+	{
+		if (bits.Find(Vet2[i]) == true)
+		{
+			cout << "误判错误" << endl;
+		}
+	}
+}
+
+int main()
+{
+	//Test();
+	Test2();
+	return 0;
+}
